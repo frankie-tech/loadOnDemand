dc3356b7f6cb12aec22a9e1ca4f0e0f0ea761509
Connor Wilson <26461046+frankiekie@users.noreply.github.com>
2019-09-27 19:49:33 -0400

    changed more over to class methods, debating switching back to more functional code seems to be getting out of hand

--- js/loadOnDemand.js
+++ js/loadOnDemand.js
@@ -34,17 +34,53 @@ function load(options) {
 
 class Load {
     constructor() {
-
+        this.head = document.head;
+        this.body = document.body;
     }
-
-    static el(tag) {
-        return document.createElement(el);
+    /* shorthand methods */
+    static elCreate(tag) {
+        return (document.createElement(tag), {
+            attrs: (attr = {}) => Object.assign(el, attr)
+        });
     }
 
     static append(parent, el) {
         return parent.appendChild(el);
     }
 
+    static attr(el, obj) {
+        return Object.assign(el, obj);
+    }
 
+    static extend(defaults, obj) {
+        for (const i in obj) {
+            if (obj.hasOwnProperty(i)) {
+                defaults[i] = obj[i]
+            }
+        }
+        return defaults;
+    }
+
+    /* Designed to build a script or link tag */
+    buildEl(tag, attributes = {}) {
+        return this.extend(this.elCreate(tag), attributes);
+    }
+
+    /*
+    * Adds eventListener to a target
+    * goal is to give a callback that will
+    * then load the script after a specific
+    * event is fired
+    */
+    static addEvent(cb, target = document, event = 'click') {
+        return target.addEventListener(event, cb);
+    }
+
+    didThatJustWork() {
+        this.buildEl('script', {});
+    }
 }
 
+const loadthis = new Load();
+
+loadthis.didThatJustWork();
