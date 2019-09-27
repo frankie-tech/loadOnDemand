2ee4d34316f5d2a75f19fd08ee52fe50deddc4bf
Connor Wilson <26461046+frankiekie@users.noreply.github.com>
2019-09-27 18:43:26 -0400

    began movement over to class architecture

--- js/loadOnDemand.js
+++ js/loadOnDemand.js
@@ -1,9 +1,50 @@
-function loadScriptOnDemand(attributes, resolve, reject) {
-    return new Promise(function (resolve, reject) {
-        var scriptTag = document.createElement('script');
-        Object.assign(scriptTag, attributes);
-        document.head.appendChild(scriptTag);
-    })
+/*
+function loadOnDemand(options) {
+    return new Promise(() => createScript(options))
         .then(resolve)
         .catch(reject);
 }
+
+function createScript(attr) {
+    const tag = document.createElement('script');
+    Object.assign(tag, attr);
+    return document.head.appendChild(tag);
+}
+
+const defaults = {
+    dev: true,
+    script: '',
+    link: '',
+    get resolve() {
+        console.log('Success');
+    },
+    get reject() {
+        console.error('Review code: there has been an error');
+    }
+}
+
+function load(options) {
+    const pub = {};
+    const priv = {};
+
+    priv.createEl = (tag)=> document.createElement(tag);
+
+}
+*/
+
+class Load {
+    constructor() {
+
+    }
+
+    static el(tag) {
+        return document.createElement(el);
+    }
+
+    static append(parent, el) {
+        return parent.appendChild(el);
+    }
+
+
+}
+
